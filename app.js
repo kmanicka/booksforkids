e48f8a4c1fd44b801f68db12e81c131e5e86b1ef
Kumaresan <kumaresan.manickavelu@gmail.com>
2017-02-14 16:32:09 -0800

    basic ebay api call

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var EBayBuyApi = require('e_bay_buy_api');
+
 
 var index = require('./routes/index');
 var browse = require('./routes/browse');
@@ -17,6 +19,29 @@ app.set('view engine', 'hbs');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
+
+
+// get the env variables
+app.use(function(req, res, next) {
+  // Token has special charecters so its enclosed by quotes in ENV. 
+  var authToken;
+  if(process.env.EBAY_SUPER_AUTH_TOKEN){
+  	authToken = process.env.EBAY_SUPER_AUTH_TOKEN.replace(/"/g,'');
+  }
+
+  req.eBayConfig = {
+	  "isSandbox" : process.env.EBAY_SANDBOX || true,
+	  "clientId" : process.env.EBAY_CLIENT_ID,
+	  "clientSecret" : process.env.EBAY_CLIENT_SECRET,
+	  "ruName" : process.env.EBAY_RUNAME,
+	  "superAuthToken" : authToken  	
+  }  
+
+  next()
+});
+
+
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -24,6 +49,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/browse', browse); 
+
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/browse.js
+++ routes/browse.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
+var async = require('async');
+var EBayBuyApi = require('e_bay_buy_api');
+
 
 /* GET item. */
-router.get('/item', function(req, res, next) {
-  res.render('item', { title: 'Express' });
+router.get('/item/:itemId', function(req, res, next) {
+
+    var itemId = req.params.itemId;
+
+	var api = new EBayBuyApi.BrowseApi()
+
+	// setting up the Authorization for the api call. 
+	EBayBuyApi.ApiClient.instance.authentications['OauthSecurity'].apiKey = req.eBayConfig.superAuthToken;
+
+    async.waterfall([
+	        function(next) {
+	        	 console.log('Calling getItem API ' + itemId);
+				 api.getItem(itemId, next);
+	        }
+	    ],
+       	function(error,data,response,next) { 
+
+	        if (error) {
+	            res.render('error',{message:  error.response.text});
+	        } else {
+			    res.render('item', { title: data.title });
+	        }
+
+	    }); 
 });
 
 /* GET search. */
