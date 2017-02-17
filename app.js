4876225a9063c8d83170ce27c8f27c63a9e49d80
Kumaresan Manickavelu <kumaresan.manickavelu@gmail.com>
2017-02-16 19:56:40 -0800

    complete search page

--- app.js
+++ app.js
@@ -18,8 +18,8 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+
 app.use(logger('dev'));
 
 
@@ -29,6 +29,9 @@ app.use(logger('dev'));
 /**************************************************************/
 
 app.use(function(req, res, next) {
+
+  console.log('eBay request filter');
+
   // Token has special charecters so its enclosed by quotes in ENV. 
   var authToken;
   if(process.env.EBAY_SUPER_AUTH_TOKEN){
@@ -52,6 +55,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+// uncomment after placing your favicon in /public
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
 app.use('/', index);
 app.use('/browse', browse); 
--- routes/browse.js
+++ routes/browse.js
@@ -6,7 +6,6 @@ var EBayBuyApi = require('e_bay_buy_api');
 /**********************************************************/
 // Home Page
 /**********************************************************/
-
 router.get('/home', function(req, res, next) {
   res.render('browse/home', { title: 'Express' });
 });
@@ -15,7 +14,6 @@ router.get('/home', function(req, res, next) {
 /**********************************************************/
 // Item Group Page
 /**********************************************************/
-
 router.get('/item_group', function(req, res, next) {
   res.render('browse/item_group', { title: 'Express' });
 });
@@ -24,20 +22,96 @@ router.get('/item_group', function(req, res, next) {
 /**********************************************************/
 // Search Page
 /**********************************************************/
-
 router.get('/search', function(req, res, next) {
-  res.render('browse/search', { title: 'Express' });
+
+  async.waterfall([
+        function(next) {
+			  var q = req.query.q;
+              console.log('Calling search API ' + q);
+
+			  /********************************************************/
+			  // start setup filters
+			  // TODO refer to following link and build a better search filtering experience. 
+			  // http://developer.ebay.com/devzone/rest/api-ref/browse/search-filters.html
+			  // setup the filters  
+			  var filter = [];
+
+			  // processing the condition filter
+			  if(req.query.condition) {
+			  	filter.push('conditions:' + req.query.condition);
+			  }			  
+
+			  // processing the price filter.
+			  if(req.query.price){
+			  	filter.push('price:' + req.query.price);
+			  	filter.push('priceCurrency:USD');
+			  }
+			  // end setup filters
+			  /********************************************************/
+
+			  var opts = { 
+				  'categoryIds': "", // String | category id
+				  'limit': req.query.limit, // String | Limit for Pagination
+				  'offset': req.query.offset, // String | Offset for Pagination
+				  'filter': filter, // [String] | Filter Field
+				  'sort': [] // [String] | Sort Field
+				};
+
+			  console.log(filter);
+
+			  var api = new EBayBuyApi.BrowseApi()
+			  // setting up the Authorization for the api call. 
+			  EBayBuyApi.ApiClient.instance.authentications['OauthSecurity'].apiKey = req.eBayConfig.superAuthToken;
+
+			  api.searchForItems(q, opts, next);
+        }
+    ],
+   	function(error,data,response,next) {
+
+        if (error) {
+
+            res.render('error',{message:  error.response.text});
+
+        } else {
+
+        	/*****************************************************/
+        	// post process response elements for web presentation
+
+        	// prepare the previous url offset
+        	if(data.prev){
+        		var prev_offset = parseInt(req.query.offset) - parseInt(req.query.limit);
+        		data.prev_url = 'q=' + req.query.q + '&limit=' + req.query.limit + '&offset=' + prev_offset;	        	
+	        	if(req.query.condition) data.prev_url += '&condition=' + req.query.condition;
+				if(req.query.price) data.prev_url += '&price=' + req.query.price;				  
+			}
+
+        	// prepare the next url offset
+        	if(data.next){
+        		var next_offset = parseInt(req.query.offset) + parseInt(req.query.limit);
+        		data.next_url = 'q=' + req.query.q + '&limit=' + req.query.limit + '&offset=' + next_offset;	        	
+	        	if(req.query.condition) data.next_url += '&condition=' + req.query.condition;
+				if(req.query.price) data.next_url += '&price=' + req.query.price;				  
+        	}
+        	/*****************************************************/
+
+        	var local = {};
+        	local.data = data;
+        	local.req = req;
+
+        	console.log('Data : ' + data.href)
+		    res.render('browse/search', local);
+        }
+
+    }); 
 });
 
 
 /**********************************************************/
 // View Item Page
 /**********************************************************/
-
 router.get('/item/:itemId', function(req, res, next) {
 
     var itemId = req.params.itemId;
-
 	var api = new EBayBuyApi.BrowseApi()
 
 	// setting up the Authorization for the api call. 
@@ -61,3 +135,4 @@ router.get('/item/:itemId', function(req, res, next) {
 });
 
 module.exports = router;
+
